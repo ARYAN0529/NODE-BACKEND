c8db9f13b5a7b688c2fc043773b4578ba9b2d916
Aryan <aryandz0529@gmail.com>
2026-06-02 23:47:13 +0530

    improve UI

--- 05_authentication/app.js
+++ 05_authentication/app.js
@@ -1,26 +1,34 @@
-const cookieParser = require('cookie-Parser');
-const express = require ('express');
-const app= express();
+const cookieParser = require("cookie-Parser");
+const express = require("express");
+const app = express();
 app.use(cookieParser());
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 // app.get('/',function (req , res) {
-//     res.cookie('name','Aryan'); //set kar rahe hai 
+//     res.cookie('name','Aryan'); //set kar rahe hai
 //     res.send("hello world");
 // })
 
-app.get('/' ,function (req , res ){
-    bcrypt.genSalt(10, function(err, salt) {  //salt is used to generate random string
-    bcrypt.hash("password", salt, function(err, hash) {
-        console.log(hash); //  hash password encrypted
-    });
+// app.get("/", function (req, res) {
+//   bcrypt.genSalt(10, function (err, salt) {
+//     //salt is algo used to generate random string
+//     bcrypt.hash("password", salt, function (err, hash) {
+//       console.log(hash); //  hash password encrypted
+//     });
+//   });
+// });
+
+
+
+// app.get("/read", function (req, res) {
+//   console.log(req.cookies);
+//   res.send("read page ");
+// });
+
+
+app.get('/' , function (req , res ) {
+  let token = jwt.sign({email:"aryan@gmail.com"},"secret");
+    console.log(token);
 });
-})
 
-
-
-app.get('/read',function(req , res) {
-    console.log(req.cookies);
-    res.send('read page ')
-})
-
-app.listen(3000)
+app.listen(3000);
